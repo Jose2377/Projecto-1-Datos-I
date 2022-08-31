f798211e01b098206898e754835331ae80c0d7f8
Jose Maria Vindas Ortiz <josemaria130@outlook.es>
2022-08-31 15:34:38 -0600

    Base funcional

--- Extraclase1/src/application/Estudiante.java
+++ Extraclase1/src/application/Estudiante.java
@@ -4,43 +4,114 @@ public class Estudiante {
 	private String carne;
 	private String nombre;
 	private String correo;
-	private String telefono;
+	private int telefono;
 	private String nickname;
-	private String t_estudiante;
-	/*
-	private float N_promedio_exam;
-	private float N_promedio_quic;
-	private float N_promedio_tar;
-	private float N_promedio1;
-	private float N_promedio2;
-	private float N_promedio3;
-	*/
-	public Estudiante(String carne,String nombre, String correo) {
+	private String tipo;
+	private float exam;
+	private float quic;
+	private float tarea;
+	private float pro1;
+	private float pro2;
+	private float pro3;
+	private float prom1;
+	private float prom2;
+	private float promf;
+	
+	public Estudiante(String carne,String nombre, String correo, String telefono, String nickname, String tipo, String exam, String quic, String tarea, String pro1, String pro2, String pro3) {
 		this.carne = carne;
 		this.nombre = nombre;
 		this.correo = correo;
-
+		try {
+			this.telefono = Integer.parseInt(telefono);
+		} catch (Exception e)
+		{
+			this.telefono = -1;
+		}
+		this.nickname = nickname;
+		this.tipo = tipo;
+		try {
+			this.exam = Float.parseFloat(exam);
+		} catch (Exception e)
+		{
+			this.exam = -1;
+		}
+		try {
+			this.quic = Float.parseFloat(quic);
+		} catch (Exception e)
+		{
+			this.quic = -1;
+		}
+		try {
+			this.tarea = Float.parseFloat(tarea);
+		} catch (Exception e)
+		{
+			this.tarea = -1;
+		}
+		try {
+			this.pro1 = Float.parseFloat(pro1);
+		} catch (Exception e)
+		{
+			this.pro1 = -1;
+		}
+		try {
+			this.pro2 = Float.parseFloat(pro2);
+		} catch (Exception e)
+		{
+			this.pro2 = -1;
+		}
+		try {
+			this.pro3 = Float.parseFloat(pro3);
+		} catch (Exception e)
+		{
+			this.pro3 = -1;
+		}
+		this.prom1 = (this.pro1+this.pro2+this.pro3)/3;
+		this.prom2 = (this.exam+this.quic+this.tarea)/3;
+		this.promf = (this.prom1+this.prom2)/2;
 	}
     public String getCarne() {
         return carne;
     }
-
     public String getNombre() {
         return nombre;
     }
-
     public String getCorreo() {
         return correo;
     }
-    public String getTelefono() {
+    public int getTelefono() {
         return telefono;
     }
-
     public String getNickname() {
         return nickname;
     }
-
     public String getTipo() {
-        return t_estudiante;
+        return tipo;
+    }
+    public float getExam() {
+        return exam;
+    }
+    public float getQuic() {
+        return quic;
+    }
+    public float getTarea() {
+        return tarea;
+    }
+    public float getPro1() {
+        return pro1;
+    }
+    public float getPro2() {
+        return pro2;
+    }
+    public float getPro3() {
+        return pro3;
+    }
+    public float getProm1() {
+        return prom1;
+    }
+    public float getProm2() {
+        return prom2;
+    }
+    public float getPromf() {
+        return promf;
     }
 }
--- Extraclase1/src/application/Main.java
+++ Extraclase1/src/application/Main.java
@@ -1,10 +1,10 @@
 package application;
-	
-
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import javafx.application.Application;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.control.TableColumn;
@@ -14,7 +14,6 @@ import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.control.Button;
 
@@ -24,13 +23,20 @@ public class Main extends Application {
     {
     	ObservableList<Estudiante> data = FXCollections.observableArrayList();
         BufferedReader br;
+        FileChooser file = new FileChooser();
+        file.setTitle("Seleccione el archivo");
+        file.getExtensionFilters().add(new FileChooser.ExtensionFilter("CSV","*.csv"));
+        File file1 = file.showOpenDialog(null);
         try {
-        	br = new BufferedReader(new FileReader("\\Users\\Jose Maria Vindas\\Desktop\\Prueba.csv"));
+        	br = new BufferedReader(new FileReader(file1));
             String line = br.readLine();
             while (line != null) {
                 String[] Posi = line.split(";", -1);
-                Estudiante est = new Estudiante(Posi[0], Posi[1], Posi[2]); 
-                data.add(est);
+                if (Posi.length > 11)
+                {
+                	Estudiante est = new Estudiante(Posi[0], Posi[1], Posi[2], Posi[3], Posi[4], Posi[5], Posi[6], Posi[7], Posi[8], Posi[9], Posi[10], Posi[11]); 
+                	data.add(est);
+                }
                 line = br.readLine();
             }
             tabla.setItems(data);
@@ -43,12 +49,11 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
     	try {
     		Pane root = new Pane();
-            Scene scene = new Scene(root, 1000, 500);
+            Scene scene = new Scene(root, 1270, 500);
             
-            HBox areaBotones = new HBox();
-            Button btn = new Button("Opcion 1");
-            btn.setLayoutX(500);
-            btn.setLayoutY(10);
+            Button btn = new Button("Abrir archivo");
+            btn.setLayoutX(600);
+            btn.setLayoutY(420);
             btn.setOnAction(new EventHandler<ActionEvent>() {
             	@Override
             	public void handle(ActionEvent event) {
@@ -63,20 +68,49 @@ public class Main extends Application {
             	}
             });
             
-    	    TableColumn<Estudiante, String> nombre = new TableColumn<>("Carne");
-            nombre.setCellValueFactory(new PropertyValueFactory<Estudiante, String>("carne"));
-            TableColumn<Estudiante, String> apellido = new TableColumn<>("Nombre");
-            apellido.setCellValueFactory(new PropertyValueFactory<Estudiante, String>("nombre"));
+    	    TableColumn<Estudiante, String> carne = new TableColumn<>("Carne");
+            carne.setCellValueFactory(new PropertyValueFactory<Estudiante, String>("carne"));
+            TableColumn<Estudiante, String> nombre = new TableColumn<>("Nombre");
+            nombre.setCellValueFactory(new PropertyValueFactory<Estudiante, String>("nombre"));
             TableColumn<Estudiante, String> email = new TableColumn<>("Correo");
             email.setCellValueFactory(new PropertyValueFactory<Estudiante, String>("correo"));
             TableColumn<Estudiante, String> tele = new TableColumn<>("Telefono");
             tele.setCellValueFactory(new PropertyValueFactory<Estudiante, String>("telefono"));
             TableColumn<Estudiante, String> nick = new TableColumn<>("Nickname");
             nick.setCellValueFactory(new PropertyValueFactory<Estudiante, String>("nickname"));
-            tabla.getColumns().addAll(nombre, apellido, email, tele, nick);
+            TableColumn<Estudiante, String> tipo = new TableColumn<>("Tipo");
+            tipo.setCellValueFactory(new PropertyValueFactory<Estudiante, String>("tipo"));
+            tipo.setPrefWidth(50);
+            TableColumn<Estudiante, String> exam = new TableColumn<>("Prom. Exam.");
+            exam.setCellValueFactory(new PropertyValueFactory<Estudiante, String>("exam"));
+            TableColumn<Estudiante, String> quiz = new TableColumn<>("Prom. Quiz");
+            quiz.setCellValueFactory(new PropertyValueFactory<Estudiante, String>("quic"));
+            TableColumn<Estudiante, String> tarea = new TableColumn<>("Prom. Tarea");
+            tarea.setCellValueFactory(new PropertyValueFactory<Estudiante, String>("tarea"));
+            TableColumn<Estudiante, String> pro1 = new TableColumn<>("Nota Proyecto 1");
+            pro1.setCellValueFactory(new PropertyValueFactory<Estudiante, String>("pro1"));
+            pro1.setPrefWidth(100);
+            TableColumn<Estudiante, String> pro2 = new TableColumn<>("Nota Proyecto 2");
+            pro2.setCellValueFactory(new PropertyValueFactory<Estudiante, String>("pro2"));
+            pro2.setPrefWidth(100);
+            TableColumn<Estudiante, String> pro3 = new TableColumn<>("Nota Proyecto 3");
+            pro3.setCellValueFactory(new PropertyValueFactory<Estudiante, String>("pro3"));
+            pro3.setPrefWidth(100);
+            TableColumn<Estudiante, String> prom1 = new TableColumn<>("Prom. Proyectos");
+            prom1.setCellValueFactory(new PropertyValueFactory<Estudiante, String>("prom1"));
+            prom1.setPrefWidth(100);
+            TableColumn<Estudiante, String> prom2 = new TableColumn<>("Prom. Trabajos");
+            prom2.setCellValueFactory(new PropertyValueFactory<Estudiante, String>("prom2"));
+            prom2.setPrefWidth(100);
+            TableColumn<Estudiante, String> promf = new TableColumn<>("Prom. Final");
+            promf.setCellValueFactory(new PropertyValueFactory<Estudiante, String>("promf"));
+            promf.setPrefWidth(80);
+            
+            tabla.getColumns().addAll(carne, nombre, email, tele, nick, tipo, exam, quiz, tarea, pro1, pro2, pro3, prom1, prom2, promf);
             
             root.getChildren().add(btn);
             root.getChildren().add(tabla);
+            primaryStage.setResizable(false);
             primaryStage.setTitle("Prueba");
             primaryStage.setScene(scene);
             primaryStage.show();
